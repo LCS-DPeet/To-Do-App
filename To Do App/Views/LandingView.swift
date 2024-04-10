2521a60cb6837ef33bb49031e18881083bcd6070
Danika Peet <DPeet@lcs.on.ca>
2024-04-10 12:01:10 -0400

    Made it possible to add a new to-do item.

--- To Do App/Views/LandingView.swift
+++ To Do App/Views/LandingView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct LandingView: View {
     
+    
+    
     //MARK: stored properties
     // the item is currently being added
     @State var newItemDescription: String = ""
+    
+    @State var todos: [TodoItem] = exampleItems
     
     // MARK: computed properties
     // the search text
     @State var searchText = ""
     
-    // the list of to-do items
-    @State var todos: [TodoItem] = exampleItems
+    //MARK: functions
+    
+    
+    //create the new list of to-do item instance
     
     var body: some View {
         
@@ -29,7 +35,9 @@ struct LandingView: View {
                 List (todos) { todo in
                     ItemView(currentItem: todo)
                     
+                    
                 }
+                
                 .searchable(text: $searchText)
                 
                 HStack {
@@ -38,8 +46,10 @@ struct LandingView: View {
                     
                     Button("ADD") {
                         // add the new to-do item
+                        createToDo(withTitle: newItemDescription)
                     }
                     .font(.caption)
+                    .disabled(newItemDescription.isEmpty == true)
                 }
                 .padding(20.0)
                 
@@ -47,10 +57,16 @@ struct LandingView: View {
             .navigationTitle("To do")
         }
     }
+    func createToDo(withTitle title: String) {
+        let todo = TodoItem (title: title, done: false)
+        
+        todos.append(todo)
+    }
 }
 
 #Preview {
     LandingView()
 }
+
 
 
